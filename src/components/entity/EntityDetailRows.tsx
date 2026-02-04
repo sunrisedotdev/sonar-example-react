aa49cf4d2f9718c729d0f80530dce148c36b14d2
Will Sewell <willsewell@echo.xyz>
2026-02-04 10:31:49 +0000

    Handle possible eu investing region

--- src/components/entity/EntityDetailRows.tsx
+++ src/components/entity/EntityDetailRows.tsx
@@ -9,22 +9,12 @@ export function EntityDetailRows({ entity }: EntityDetailRowsProps) {
     return null;
   }
 
-  let usRegion = "unknown";
-  switch (entity.InvestingRegion) {
-    case "us":
-      usRegion = "yes";
-      break;
-    case "other":
-      usRegion = "no";
-      break;
-  }
-
   return (
     <div className="flex flex-col gap-1 bg-gray-200 p-2 rounded-md w-full">
       <span className="text-gray-900">
         Type: {entity.EntityType === EntityType.USER ? "individual" : "organization"}
       </span>
-      <span className="text-gray-900">Is US investor: {usRegion}</span>
+      <span className="text-gray-900">Investing Region: {entity.InvestingRegion}</span>
     </div>
   );
 }
